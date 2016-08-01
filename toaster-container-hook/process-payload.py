c9044b52f5c6d612a76c46f01aa82770bdb645f7
Randy Witt <randy.e.witt@linux.intel.com>
2016-08-01 15:08:11 -0700

    process-payload.sh: Operate on a list of branches
    We will want to support multiple branches so let's use a list instead.
    
    Signed-off-by: Randy Witt <randy.e.witt@linux.intel.com>

--- toaster-container-hook/process-payload.py
+++ toaster-container-hook/process-payload.py
@@ -7,10 +7,14 @@ import subprocess
 
 hook_loc = os.path.dirname(os.path.realpath(__file__))
 
+important_branches = [ "halstead/tmp", "halstead/tmp2" ]
+
 with open(os.path.join(sys.argv[1], "payload")) as f:
     data = json.load(f)
 
     branches = data["branches"]
 
-    if "halstead/tmp" in branches:
-        subprocess.call(os.path.join(hook_loc, "trigger-travis.sh"))
+    for branch in important_branches:
+        if branch in branches:
+            subprocess.call(os.path.join(hook_loc, "trigger-travis.sh"))
+            break
