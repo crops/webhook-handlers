88846f8a60382e7f8d4aba729cdec2f46f3802e3
Randy Witt <randy.e.witt@linux.intel.com>
2016-08-10 14:42:26 -0700

    process-payload.py: Use urllib2 rather than calling another script
    It's much simpler to keep everything in the same script. It prevents
    having to try figuring out paths in order to run separate scripts.
    
    This also allows an easier way to pass arguments down to the travis
    webhook.
    
    Signed-off-by: Randy Witt <randy.e.witt@linux.intel.com>

--- toaster-container-hook/process-payload.py
+++ toaster-container-hook/process-payload.py
@@ -4,8 +4,31 @@ import json
 import os
 import sys
 import subprocess
+import urllib2
 
-hook_loc = os.path.dirname(os.path.realpath(__file__))
+def trigger_travis(branch, committish):
+    message = "API triggered build... {}:{}".format(branch, committish)
+
+    request = {}
+    request["message"] = message
+    request["branch"] = "master"
+
+    data = {}
+    data["request"] = request
+
+    url = "https://api.travis-ci.org/repo/crops%2Ftoaster-container/requests"
+    token = os.environ["TOASTER_CONTAINER_TOKEN"]
+    headers = {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Travis-API-Version": "3",
+                "Authorization": "token {}".format(token)
+              }
+    url_request = urllib2.Request(url, json.dumps(data), headers)
+
+    print url_request
+    urllib2.urlopen(url_request)
+
 
 important_branches = [ "master" ]
 
@@ -15,6 +38,11 @@ with open(os.path.join(sys.argv[1], "payload")) as f:
     branches = data["branches"]
 
     for branch in important_branches:
-        if branch in branches:
-            subprocess.call(os.path.join(hook_loc, "trigger-travis.sh"))
-            break
+        try:
+            index = branches.index(branch)
+        except ValueError:
+            continue
+
+        committish = data["detail"][index]
+        trigger_travis(branch, committish)
+        break
